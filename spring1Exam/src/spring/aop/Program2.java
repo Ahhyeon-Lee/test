0a7fee6817f5686c5bd35923f13b455609ae0b95
smhrd <smhrd@59.0.236.250>
2021-04-16 09:29:30 +0900

    첫번째 커밋(저장)

---
+++ spring1Exam/src/spring/aop/Program2.java
@@ -0,0 +1,30 @@
+package spring.aop;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.aop.entity.Exam;
+import spring.aop.entity.NewlecExam;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context =
+				new ClassPathXmlApplicationContext("spring/aop/setting.xml");
+		
+		Exam proxy = (Exam)context.getBean("proxy");
+		
+		System.out.printf("total is %d \n", proxy.total());
+		System.out.println("------------------------------");
+		System.out.printf("avg is %f \n", proxy.avg());
+		//System.out.printf("total is %d \n", exam.total());
+		
+
+	}
+
+}
